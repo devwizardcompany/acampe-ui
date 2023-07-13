93254eef9a12854c1036744be530d8099a00119d
Charleston Amaral <charleston.silva03@gmail.com>
2023-07-13 20:35:09 -0300

    fix(textfield): add props label & icon

--- packages/react/src/components/TextField/index.tsx
+++ packages/react/src/components/TextField/index.tsx
@@ -1,22 +1,35 @@
 import { Eye, EyeClosed } from '@phosphor-icons/react'
-import { ComponentProps, useState } from 'react'
-import { Button, Input, Wrapper } from './styles'
+import { ComponentProps, ReactNode, useState } from 'react'
+import { Button, Container, Input, Text, Wrapper } from './styles'
 
 export interface TextFieldProps extends ComponentProps<typeof Input> {
+  icon?: ReactNode
+  label?: string
   password: boolean
 }
 
-export function TextField({ password, ...props }: TextFieldProps) {
+export function TextField({ icon, label, password, ...props }: TextFieldProps) {
   const [hidden, setHidden] = useState<boolean>(true)
 
   return (
-    <Wrapper>
-      <Input type={password && hidden ? 'password' : 'text'} {...props} />
-      {password && (
-        <Button type="button" onClick={() => setHidden((state) => !state)}>
-          {hidden ? <Eye /> : <EyeClosed />}
-        </Button>
-      )}
-    </Wrapper>
+    <Container>
+      {label && <Text htmlFor={props.name}>{label}</Text>}
+
+      <Wrapper data-icon="true">
+        {icon && icon}
+
+        <Input
+          type={password && hidden ? 'password' : 'text'}
+          {...props}
+          id={props.name}
+        />
+
+        {password && (
+          <Button type="button" onClick={() => setHidden((state) => !state)}>
+            {hidden ? <Eye /> : <EyeClosed />}
+          </Button>
+        )}
+      </Wrapper>
+    </Container>
   )
 }
