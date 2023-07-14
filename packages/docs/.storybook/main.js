653e26ffe6677cfa433d842c76b872cc69d5423c
Charleston Amaral <charleston.silva03@gmail.com>
2023-07-14 08:50:18 -0300

    chore(docs): show docs component

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -5,7 +5,8 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y"
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs"
   ],
   framework: {
     name: "@storybook/react-vite",
