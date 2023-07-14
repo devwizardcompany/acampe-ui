6d46c21631e1076bbe063a1ba2a88113a3f6fe80
Charleston Amaral <charleston.silva03@gmail.com>
2023-07-14 19:28:22 -0300

    ci(docs): remove package

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -6,7 +6,6 @@ const config = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs"
   ],
   framework: {
     name: "@storybook/react-vite",
