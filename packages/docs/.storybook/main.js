10b495f7b8bce3c447c16f770934de67f377acb2
Charleston Amaral <charleston.silva03@gmail.com>
2023-08-20 21:44:43 -0300

    chore(docs): configure base

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -16,7 +16,7 @@ const config = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/acampe-ui/'
+      config.base = '/devwizardcompany/'
     }
 
     return config
